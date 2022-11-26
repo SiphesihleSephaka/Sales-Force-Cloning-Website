db9ab3cbba62bd086216aa08ec7d8458dfdff33d
Siphesihle Sephaka <sihlekozo@gmail.com>
2022-11-26 23:30:50 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,256 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <div className="App">
+    <p class="H"><b> 00800 7253 3333 (FREEPHONE) <a href ="https://www.salesforce.com/eu/company/contact-us/?d=cta-header-9"> Contact Us </a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; | &nbsp;&nbsp;&nbsp;&nbsp;  <a href ="https://login.salesforce.com/?locale=eu" > LOGIN </a> &nbsp;&nbsp;&nbsp;&nbsp;</b></p>
+   
+    <p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <img src="salesforce-cloud-logo-sm.png" width="90" height="60"/> 
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <button class="OO" >
+    <a href ="https://www.salesforce.com/eu/form/signup/freetrial-sales-pe/?d=cta-jumbotron-2-freetrial"  class="Btn"> TRY FOR FREE</a> 
+      </button>
+      </p> 
+    
+    <hr/>
+
+<p className="Home"> <img src="homepage-post-df-cost-savings-fg-new.png" width="800" height="700" align="right"/> </p>
+    
+      <h1>Drive growth with <br/>
+      magical customer <br/>
+         360 experiences.</h1>
+
+         <p class="Second"> See how Salesforce can help you unite data <br/>
+         to make every customer moment more <br/>
+         cost-efficient. </p>
+
+     <button class="B1">
+      <p> <a href ="https://www.salesforce.com/eu/form/signup/freetrial-sales-pe/?d=cta-jumbotron-2-freetrial"  class="Btn"> START MY FREE TRIAL </a> </p>
+      </button>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <button class="B2">
+      <p> <a href ="https://www.salesforce.com/eu/form/demo/demo-overview/?d=cta-jumbotron-3-demo-overview"  class="Btn"> WATCH DEMOS </a> <br/></p>
+      </button>
+
+      
+
+      <p class="link"> <br/><br/> <br/> <br/> <br/><br/>What's new at Salesforce? </p>
+
+      <p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<img src="1.png" width="120" height="120"/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SALESFORCE GENIE DEMO&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <img src="2.png" width="120" height="120"/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;RESEARCH AND REPORT&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      <img src="3.png" width="120" height="120"/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; GUIDE </p>
+
+
+<table class="TD">
+  <tr>
+    <td> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.salesforce.com/eu/form/genie/demo-video/?d=cta-mustache-1-demo-genie"><b> Introducing Salesforce Genie, the first real-time <br/> platform that helps you save money, <br/> save time, and grow revenue.</b></a> </td>
+    <td> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.salesforce.com/eu/resources/research-reports/state-of-the-connected-customer/?d=cta-mustache-2-state-connected-customer"> <b> Discover the trends shaping the future of <br/> customer engagement in the State <br/>of the Connected Customer report.</b> </a> </td>
+    <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.salesforce.com/eu/form/pdf/crm-handbook/?d=cta-mustache-3-crm-handbook"> <b> Learn everything you need to know about CRM <br/> (Customer Relationship Management) <br/> with our Complete CRM Handbook.</b> </a></td>
+  </tr>
+</table>
+
+       <p class="A"><br/><br/> Learn what Salesforce products can do for you. </p>
+
+       <p class="Two"> &nbsp;&nbsp;&nbsp;&nbsp; <img src="php-products-4.png"/> &nbsp;&nbsp;&nbsp;&nbsp; <img src="111.png"/>&nbsp;&nbsp;&nbsp;&nbsp; <img src="22.png"/> &nbsp;&nbsp;&nbsp;&nbsp;<img src="333.png"/> 
+       </p>
+
+       <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; SMALL BUSINESS  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        SALES &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        SERVICE&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        SEE ALL PRODUCTS
+       </h3>
+
+       <p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b> Sell smater and provide support &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Close more deals and speed up &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Make customers happy faster &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Dig into the full range of 
+        </b> </p>
+
+        <p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b> faster in a single app. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; growth with the #1 CRM. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; and build loyalty with Service Cloud. &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Customer 360 offerings.
+        </b> </p>
+
+        <p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <button class="WD1"> <a href ="https://www.salesforce.com/eu/form/demo/demo-small-business/?d=cta-democards-1-demo-smb_demo"  class="Btn"> WATCH DEMOS </a> </button> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+          <button class="WD2"> <a href ="https://www.salesforce.com/eu/form/demo/demo-sales/?d=cta-democards-3-demo-sales_demo"  class="Btn"> WATCH DEMOS </a> </button> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+          <button class="WD3"> <a href ="https://www.salesforce.com/eu/form/demo/demo-service/?d=cta-democards-5-demo-service_demo"  class="Btn"> WATCH DEMOS </a> </button> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+          <button class="WD4"> <a href ="https://www.salesforce.com/eu/form/demo/demo-overview/?d=cta-democards-7-demo-products_demo"  class="Btn"> WATCH DEMOS </a> </button>
+        </p>
+
+        <p>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.salesforce.com/eu/solutions/small-business-solutions/overview/?d=cta-democards-2-ungated-smb_learn"> <b class="F"> Learn more &gt; </b> </a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+         
+        <a href="https://www.salesforce.com/eu/products/sales-cloud/overview/?d=cta-democards-4-ungated-sales_learn"> <b class="F"> Learn more &gt; </b> </a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+        <a href="https://www.salesforce.com/eu/products/service-cloud/overview/?d=cta-democards-6-ungated-service_learn"> <b class="F"> Learn more &gt; </b> </a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+        <a href="https://www.salesforce.com/eu/form/demo/demo-overview/?d=cta-democards-7-demo-products_demo"> <b class="F"> Learn more &gt; </b> </a>
+
+        </p>
+
+       <p class="Last"> <br/>Find a perfectly tailored solution for any industry. </p>
+
+       <p> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+        <button class="B3">
+      <p> <a href ="https://www.salesforce.com/eu/solutions/industries/?d=cta-democards-9-ungated-industries_explore"  class="x3"> EXPLORE NOW </a> </p>
+      </button>
+      </p>
+
+  <p> <br/> <br/> <br/><br/><br/><br/> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  <a href="https://www.salesforce.com/eu/"> <img src="salesforce-cloud-logo-sm.png" width="90" height="60"/> </a> </p> 
+      <p>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.facebook.com/salesforce"> <img src="facebook.svg" width="30" height="30"/> </a>
+       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://twitter.com/salesforce"> <img src="twitter.svg" width="30" height="30"/> </a>
+       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.linkedin.com/company/salesforce"> <img src="linkedin.svg" width="30" height="30"/> </a> 
+       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="https://www.youtube.com/user/SalesforceUK"> <img src="youtube.svg" width="30" height="30"/> </a> 
+
+
+<table class="center" align="center" cellspacing="3" cellpadding="6">
+  <tr>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <th>New to Salesforce? </th>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <th>Popular Links</th>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <th>About Salesforce</th>
+  </tr>
+
+  <tr>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://www.salesforce.com/eu/products/what-is-salesforce/"> What is Salesforce?</a>
+    </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://www.salesforce.com/eu/solutions/small-business-solutions/overview/"> Small Business CRM </a>
+    </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://www.salesforce.com/eu/company/our-story/"> Our Story </a>
+     </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  </tr>
+
+  <tr>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://www.salesforce.com/eu/learning-centre/crm/what-is-crm/"> What is CRM? </a>
+      </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://www.salesforce.com/eu/products/sales-cloud/overview/"> Sales Force Automation </a>
+      </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://www.salesforce.com/eu/company/careers/rdr/"> Careers </a>
+    </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  </tr>
+
+  <tr>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td>
+      <a href ="https://www.salesforce.com/eu/learning-centre/tech/cloudcomputing/"> What is Cloud Computing? </a>
+      </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://www.salesforce.com/eu/products/service-cloud/overview/"> Customer Service Solutions </a>
+      </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://www.salesforce.com/uk/news/">Press </a>
+    </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  </tr>
+
+  <tr>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td>
+    <a href ="https://www.salesforce.com/eu/products/"> CRM Solutions </a> 
+    </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://www.salesforce.com/eu/products/marketing-cloud/overview/"> Digital Marketing Solutions </a>
+    </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://trust.salesforce.com/en/"> Security and Performance </a>
+    </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  </tr>
+
+  <tr>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://www.salesforce.com/eu/customer-success-stories/"> Customer Success Stories </a>
+    </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://www.salesforce.com/eu/solutions/industries/"> Industry Solutions </a>
+    </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://www.salesforce.com/company/legal/"> Legal </a>
+    </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  </tr>
+
+  <tr>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://www.salesforce.com/eu/editions-pricing/sales-cloud/"> Product pricing </a>
+    </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://www.salesforce.com/eu/events/#!page=1"> Salesforce Events </a> 
+    </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://www.salesforce.com/privacy/overview/"> Privacy for Salesforce Products </a> 
+    </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  </tr>
+
+  <tr>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://www.salesforce.com/eu/form/contact/contactme/"> Contact Us </a>
+    </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://www.salesforce.com/products/innovation/winter-23-release/?sfdc-redirect=128"> New Release Features </a>
+    </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> 
+    <a href ="https://www.salesforce.com/eu/blog"> Salesforce EU Blog </a>
+    </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  </tr>
+
+  <tr>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td> </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td>
+    <a href ="https://www.salesforce.com/eu/campaign/your-account/"> Manage Subscription </a>
+    </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <td>
+    <a href ="https://www.salesforce.com/eu/form/other/blog-newsletter/"> Salesforce EU Blog Signup </a>
+    </td>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  </tr>
+</table>
+
+
+</p>
+
+
+  </div>
+        
+
+
+  )
+}
+
+export default App
